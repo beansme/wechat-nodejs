c7deb9feffe3b7d76ef8731afcbd659c8e2d4c1c
troy <lizeguangok@gmail.com>
2014-05-04 14:29:21 +0800

    server test

--- routes/wechat.js
+++ routes/wechat.js
@@ -11,142 +11,19 @@ router.get('/:wechat_token', wechat('szu_token', function(req, res, next){
 
 
 router.post('/:wechat_token', wechat('szu_token', wechat.text(function (message, req, res, next) {
-        //test data
-        var message = {ToUserName: 'gh_44e8155eca9d',FromUserName: 'o094_t0a2KTqu2OKLHgYlgoi2j_0',CreateTime: '1359125035',MsgType: 'text',Content: 'test',MsgId: '5837397576500011341'};
-
-        var wechatKeyword = message.Content;
-        var client = req.client
-        var Keyword = require('../model/keyword');
-
-        Keyword.handle(message, client, function(err, done){
-            if(err) {
-                console.log(err);
-            }else if(!done) {
-                var Reply = require('../model/reply');
-                Reply.handle('text', message, req.client, function(err, done){
-                    if(err){
-                        console.log(err);
-                    } else {
-                        if(!done) {
-                            redisClient.incr(client.client_id+':new.message');
-                        }
-                    }
-                });
-            } else {
-            }
-        });
-        res.writeHead(200);
-        res.end();
-
-    }).image(function (message, req, res, next) {
-        var Reply = require('../model/reply');
-        Reply.handle('image', message, req.client, function(err, done){
-            if(err){
-                console.log(err);
-            } else {
-                if(!done) {
-                    redisClient.incr(req.client.client_id+':new.message');
-                }
-            }
-        });
-        res.writeHead(200);
-        res.end();
-      // message为图片内容
-      // { ToUserName: 'gh_d3e07d51b513',
-      // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
-      // CreateTime: '1359124971',
-      // MsgType: 'image',
-      // PicUrl: 'http://mmsns.qpic.cn/mmsns/bfc815ygvIWcaaZlEXJV7NzhmA3Y2fc4eBOxLjpPI60Q1Q6ibYicwg/0',
-      // MediaId: 'media_id',
-      // MsgId: '5837397301622104395' }
+      var message = req.weixin;
+       if (message.FromUserName === '屌丝') {
+         // 回复屌丝(普通回复)
+         res.reply('hehe');
+     }
     }).voice(function (message, req, res, next) {
-        var Reply = require('../model/reply');
-        Reply.handle('voice', message, req.client, function(err, done){
-            if(err){
-                console.log(err);
-            } else {
-                if(!done) {
-                    redisClient.incr(req.client.client_id+':new.message');
-                    
-                }
-            }
-        });
-        res.writeHead(200);
-        res.end();
-      // message为音频内容
-      // { ToUserName: 'gh_d3e07d51b513',
-      // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
-      // CreateTime: '1359125022',
-      // MsgType: 'voice',
-      // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
-      // Format: 'amr',
-      // MsgId: '5837397520665436492' }
+     
     }).video(function (message, req, res, next) {
-        var Reply = require('../model/reply');
-        Reply.handle('location', message, req.client, function(err, done){
-            if(err){
-                console.log(err);
-            } else {
-                if(!done) {
-                    redisClient.incr(req.client.client_id+':new.message');
-                }
-            }
-        });
-        res.writeHead(200);
-        res.end();
-      // message为视频内容
-      // { ToUserName: 'gh_d3e07d51b513',
-      // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
-      // CreateTime: '1359125022',
-      // MsgType: 'video',
-      // MediaId: 'OMYnpghh8fRfzHL8obuboDN9rmLig4s0xdpoNT6a5BoFZWufbE6srbCKc_bxduzS',
-      // ThumbMediaId: 'media_id',
-      // MsgId: '5837397520665436492' }
+       
     }).location(function (message, req, res, next) {
-        var Reply = require('../model/reply');
-        Reply.handle('location', message, req.client, function(err, done){
-            if(err){
-                console.log(err);
-            } else {
-                if(!done) {
-                    redisClient.incr(req.client.client_id+':new.message');
-                }
-            }
-        });
-        res.writeHead(200);
-        res.end();
-      // message为位置内容
-      // { ToUserName: 'gh_d3e07d51b513',
-      // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
-      // CreateTime: '1359125311',
-      // MsgType: 'location',
-      // Location_X: '30.283950',
-      // Location_Y: '120.063139',
-      // Scale: '15',
-      // Label: {},
-      // MsgId: '5837398761910985062' }
+      
     }).link(function (message, req, res, next) {
-        var Reply = require('../model/reply');
-        Reply.handle('link', message, req.client, function(err, done){
-            if(err){
-                console.log(err);
-            } else {
-                if(!done) {
-                    redisClient.incr(req.client.client_id+':new.message');
-                }
-            }
-        });
-        res.writeHead(200);
-        res.end();
-      // message为链接内容
-      // { ToUserName: 'gh_d3e07d51b513',
-      // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
-      // CreateTime: '1359125022',
-      // MsgType: 'link',
-      // Title: '公众平台官网链接',
-      // Description: '公众平台官网链接',
-      // Url: 'http://1024.com/',
-      // MsgId: '5837397520665436492' }
+       
     }).event(function (message, req, res, next) {
         var event = message.Event;
         var openid = message.FromUserName;
