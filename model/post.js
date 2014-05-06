c06a1a6befb8714c924cc7c867cb6e02597e58af
troy <lizeguangok@gmail.com>
2014-05-06 23:55:31 +0800

    test

--- model/post.js
+++ model/post.js
@@ -59,7 +59,9 @@ PostDAO.prototype.saveImage = function(post_id, type, media_id, callback){
 					if(type === 'banner') {
 						obj[0].banner = link;
 					} else {
-						obj[0].content.push(link);
+						Post.update({post_id: post_id}, {$push: {content:[{link}]}} ,function(err, obj){
+								
+						});
 					}
 					obj[0].save();
 					callback(err, link);
@@ -70,9 +72,8 @@ PostDAO.prototype.saveImage = function(post_id, type, media_id, callback){
 };
 
 PostDAO.prototype.saveContent = function(post_id, content, callback){
-	Post.update({post_id: post_id}, {$pushAll: {content:[content]}}, {upsert:true},function(err, obj){
+	Post.update({post_id: post_id}, {$push: {content:[{content}]}} ,function(err, obj){
 		callback(err, obj);
-
 	});
 };
 
